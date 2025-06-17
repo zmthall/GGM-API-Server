de0b23dc04d5fa91d5f55daddc1feba7c7752870
zmthall <zmthall@gmail.com>
2025-06-17 13:57:10 -0600

    Testing if this fixes directory issues

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ import { errorHandler } from './middlewares/errorHandler';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
-app.use(express.static(path.join(process.cwd(), 'static')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use(express.static(path.join(__dirname, '../static')));
 app.use('/api/media', mediaRouter);
 app.use('/api/events', eventRouter)
 app.use(errorHandler);
@@ -19,5 +19,9 @@ app.use(errorHandler);
 app.get('/', (req, res) => {
     res.status(403).send('<h1>Access is not Available.</h1>');
 })
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK', message: 'Server is running' });
+});
 
 export default app;
