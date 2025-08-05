8aa0c5da8be47fbd62045c40615a558b2b2f91e3
zmthall <zmthall@gmail.com>
2025-08-05 13:29:21 -0600

    Updated event endpoints to work with firebaseTokens instead of an api key.

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -11,24 +11,27 @@ import {
 } from '../controllers/event.controller';
 
 // Middleware
-import { authenticateKey } from '../middlewares/authenticateKey';
+import { verifyFirebaseToken } from '../middlewares/verifyFirebaseToken';
 
 const router = Router();
 
-// POST - http://127.0.0.1:4000/api/events | Adds a new event to the firebase database
+// Normal User Routes
 // GET -  http://127.0.0.1:4000/api/events | Fetches all events from the firebase database with pagination and 5 events per page
 // GET -  http://127.0.0.1:4000/api/events/archived | Fetches all archived events from the firebase database with pagination and 10 events per page
 // GET - http://127.0.0.1:4000/api/events/:id | Fetches specific events by UUID
+router.get('/', getAllEvents);
+router.get('/archived', getArchivedEvents);
+router.get('/:id', getEventById);
+
+// Administrative API Calls, requires firebaseToken for authentication
+// POST - http://127.0.0.1:4000/api/events | Adds a new event to the firebase database
 // PUT - http://127.0.0.1:4000/api/events/:id | Updates specific event by UUID
 // DELETE - http://127.0.0.1:4000/api/events/:id | Deletes specific event by UUID (permanent delete)
 // PUT - ARCHIVE - http://127.0.0.1:4000/api/events/:id/archive | Deletes and event by archiving it by UUID (temporary delete)
-router.post('/', authenticateKey, createEvent);
-router.get('/', getAllEvents);
-router.get('/archived', getArchivedEvents)
-router.get('/:id', getEventById);
-router.put('/:id', authenticateKey, updateEvent);
-router.delete('/:id', authenticateKey, deleteEvent);
-router.put('/:id/archive', authenticateKey, archiveEvent); // archive/unarchive
+router.post('/', verifyFirebaseToken, createEvent);
+router.put('/:id', verifyFirebaseToken, updateEvent);
+router.delete('/:id', verifyFirebaseToken, deleteEvent);
+router.put('/:id/archive', verifyFirebaseToken, archiveEvent); // archive/unarchive
 
 router.get('/route/health', (req, res) => {
     res.status(200).json({ status: 'OK', message: 'Event Routes are working.' });
